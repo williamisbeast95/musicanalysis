e1c546724834982b1f263550a2cbdd12af3dabe1
Noban130 <mobiusnoban130@gmail.com>
2024-09-24 16:30:57 +0900

    Update main.py - read mp3 file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import http.client
-import json
-import base64
 import os
 # Path to your local MP3 file
 file_path = 'audio/song.mp3'
@@ -8,12 +6,6 @@ file_path = 'audio/song.mp3'
 # Read the MP3 file and encode it to base64
 with open(file_path, "rb") as mp3_file:
     file_content = mp3_file.read()
-    encoded_file = base64.b64encode(file_content).decode("utf-8")
-
-# Create the payload with the base64-encoded MP3 file
-payload = json.dumps({
-    "file": encoded_file
-})
 
 headers = {
     'x-rapidapi-key': os.getenv('Sonoteller_API_KEY'),
@@ -23,7 +15,7 @@ headers = {
 
 # Send the request
 conn = http.client.HTTPSConnection("sonoteller-ai1.p.rapidapi.com")
-conn.request("POST", "/music", payload, headers)
+conn.request("POST", "/music", body=file_content, headers=headers)
 
 res = conn.getresponse()
 data = res.read()
