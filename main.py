da1b2ce024bf3d3edccdb628a5f7b4db17aef31e
Noban130 <mobiusnoban130@gmail.com>
2024-09-24 16:18:53 +0900

    update main.py env read property

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import http.client
+import json
+import base64
+import os
+# Path to your local MP3 file
+file_path = 'audio/song.mp3'
 
-conn = http.client.HTTPSConnection("sonoteller-ai1.p.rapidapi.com")
+# Read the MP3 file and encode it to base64
+with open(file_path, "rb") as mp3_file:
+    file_content = mp3_file.read()
+    encoded_file = base64.b64encode(file_content).decode("utf-8")
 
-payload = "{\"file\":\"https://storage.googleapis.com/musikame-files/thefatrat-mayday-feat-laura-brehm-lyriclyrics-videocopyright-free-music.mp3\"}"
+# Create the payload with the base64-encoded MP3 file
+payload = json.dumps({
+    "file": encoded_file
+})
 
 headers = {
-    'x-rapidapi-key': "4e1056282cmsh8fdbd0c320caf21p1e1b3cjsnd7c046c55c93",
+    'x-rapidapi-key': os.getenv('Sonoteller_API_KEY'),
     'x-rapidapi-host': "sonoteller-ai1.p.rapidapi.com",
     'Content-Type': "application/json"
 }
 
+# Send the request
+conn = http.client.HTTPSConnection("sonoteller-ai1.p.rapidapi.com")
 conn.request("POST", "/music", payload, headers)
 
 res = conn.getresponse()
